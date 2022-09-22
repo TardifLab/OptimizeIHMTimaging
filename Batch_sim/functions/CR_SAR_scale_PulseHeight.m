452e05e022306925ae2b9fafbc29ad36c35dc43a
Christopher Rowley <chris_rowley18@hotmail.com>
2022-09-22 17:19:34 -0400

    Update CR_SAR_scale_PulseHeight.m
    Fix for values that shouldnt happen

--- Batch_sim/functions/CR_SAR_scale_PulseHeight.m
+++ Batch_sim/functions/CR_SAR_scale_PulseHeight.m
@@ -9,6 +9,7 @@ function Params = CR_SAR_scale_PulseHeight(Params)
 % flip = flip angle of excitation pulses (in degrees) 
 % TR = time (in seconds)
 
+
 if ~isfield(Params,'B0') % if not defined, assume 3T
     Params.B0 = 3; % main field strength (in Tesla)
 end
@@ -17,19 +18,19 @@ if ~isfield(Params, 'boosted')
     error ('Please specify if boosted sat scheme is used (enter Params.boosted = 0 or 1)')
 end
 
-if Params.B0 == 7
-    if strcmp(Params.TransmitCoil, 'STX')
-        SAR_limit = 2.35; % Empirical value to match what I get at scanner
-    end
-else
-    SAR_limit = 3; %(W/kg)
-end
+% if Params.B0 == 7
+%     if strcmp(Params.TransmitCoil, 'STX')
+%         SAR_limit = 2.35; % Empirical value to match what I get at scanner
+%     end
+% else
+%     SAR_limit = 3; %(W/kg)
+% end
+
+SAR_limit = 3; %(W/kg)
     
 %% Multiply all variables except for the B1 field
 % Using equation from Ibrahim, Tamer S. 2004. 
 % Specify variables
-r = 0.076; % meters for human head
-l = 0.2; % meters for human head
 w = 42.58e6 *Params.B0;
 
 % There is likely time involved in the SAR monitoring for heat diffusion. A
@@ -37,6 +38,9 @@ w = 42.58e6 *Params.B0;
 % protocols based on longer vs shorter TRs
 
 if Params.B0 == 3
+    r = 0.1; % meters for human head
+    l = 0.256; % meters for human head
+
     % S/m from McCann et al 2019; 
     %conduc => % CSF = 1.71, GM is 0.466, WM is 0.21;
     if Params.boosted % modify for different definition of numSatPulse
@@ -44,14 +48,21 @@ if Params.B0 == 3
         % conduc = 0.81; % empirical value TR = 1.14
         conduc = 0.079*Params.TR + 0.72; % line fit to the above
     else
-        conduc = 1.032; % empirical value TR = 0.12
+        conduc = 1.76; % empirical value TR = 0.12
     end
 elseif Params.B0 == 7
+
+    r = 0.066; % meters for human head
+    l = 0.22; % meters for human head
+
     % From Van Lier et al 2014, the average conductivity at 7T was 
     % 13 percent higher than at 3T, when looking at GM,WM and CSF
     %conduc = 1.71*1.13;
-    conduc = 1.48; % empirically solved based on what I get at scanner.
+    conduc = 0.8; % empirically solved based on what I get at scanner.
 elseif Params.B0 == 1.5
+    r = 0.1; % meters for human head
+    l = 0.256; % meters for human head
+
     % From Van Lier et al 2014, the average conductivity at 1.5T was 
     % 10 percent lower than at 3T, when looking at GM,WM and CSF
     conduc = 1.71*0.9;
@@ -63,21 +74,23 @@ P_c = pi*r^4 * l * w^2 *conduc;
 %% Power = J/s. Multiply by pulse time to find J of work done
 % For Excitation pulse
 % numberSatPulses *(powerCoefficient*B1field) * time pulse
-excB1 = (Params.flipAngle*pi/180) / (42.58e6 * Params.WExcDur);
+excB1 = (Params.flipAngle) / (360*42.58e6 * Params.WExcDur);
 J_exc = Params.numExcitation*(P_c * excB1^2)* Params.WExcDur;
 
 %% Combine the work, divide by TR
 % Head SAR restriction is ~ 3 W/kg for head
+% Edge case that we use all the SAR up for excitation, check for negative
+checkNeg = (SAR_limit*Params.TR - J_exc);
 
-
-if Params.boosted % modify for different definition of numSatPulse
-    SatPulseNumberTotal = Params.numSatPulse * Params.satTrainPerBoost;
-    Params.satRMS = abs(sqrt((SAR_limit*Params.TR - J_exc)/ (SatPulseNumberTotal*P_c*Params.pulseDur)));
+if checkNeg < 0
+    Params.satRMS = -1;
 else
-    Params.satRMS = abs(sqrt((SAR_limit*Params.TR - J_exc)/(Params.numSatPulse*P_c*Params.pulseDur)));
+    if Params.boosted % modify for different definition of numSatPulse
+        SatPulseNumberTotal = Params.numSatPulse * Params.satTrainPerBoost;
+        Params.satRMS = sqrt((SAR_limit*Params.TR - J_exc)/ (SatPulseNumberTotal*P_c*Params.pulseDur));
+    else
+        Params.satRMS = sqrt((SAR_limit*Params.TR - J_exc)/(Params.numSatPulse*P_c*Params.pulseDur));
+    end
 end
-
-
-
 
 
